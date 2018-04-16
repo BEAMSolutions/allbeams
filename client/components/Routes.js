639eeef210b8f7fe512e981121444c59e95533c3
Beno Suriano <bjsuriano@gmail.com>
2018-04-16 17:47:22 -0600

    Fixed linter.

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {Switch, Route} from 'react-router-dom'
-import {AuthRoute, Login, Signup} from './auth'
+import { Switch, Route } from 'react-router-dom'
+import { AuthRoute, Login, Signup } from './auth'
 import Welcome from './Welcome'
 import Home from './Home'
 import NoMatch from './NoMatch'
 
 const Routes = () => (
-  <div className='fill-xy center-xy column'>
+  <div className="fill-xy center-xy column">
     <Switch>
-      <Route exact path='/' component={Welcome} />
-      <Route path='/login' component={Login} />
-      <Route path='/signup' component={Signup} />
-      <AuthRoute path='/home' component={Home} />
+      <Route exact path="/" component={Welcome} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <AuthRoute path="/home" component={Home} />
       <Route component={NoMatch} />
     </Switch>
   </div>
