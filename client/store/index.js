8c32bfcf54705945bd9c0a825dd75fac5d26de9b
eugenepark2014 <eugenepark2014@gmail.com>
2018-04-19 18:31:07 -0400

    commit

--- client/store/index.js
+++ client/store/index.js
@@ -4,25 +4,19 @@ import thunks from 'redux-thunk'
 import axios from 'axios'
 import history from '../history'
 import user from './user'
-<<<<<<< HEAD
-import products from './products'
-import categories from './categories'
-
-const reducer = combineReducers({ user, products, categories })
-=======
 import product from './product'
 import products from './products'
 import reviews from './review'
 import users from './users'
+import categories from './categories'
 
-const reducer = combineReducers({users, user, products, reviews, product})
->>>>>>> master
+const reducer = combineReducers({users, user, products, reviews, product, categories})
 
 const store = createStore(
   reducer,
   applyMiddleware(thunks.withExtraArgument({ axios, history }), logger)
 )
- 
+
 export default store
 export * from './user'
 export * from './products'
