ebc42a66c864da9e8f331d019dae14121be6be90
eugenepark2014 <eugenepark2014@gmail.com>
2018-04-19 19:04:26 -0400

    pushing again

--- client/store/products.js
+++ client/store/products.js
@@ -1,24 +1,48 @@
+//action types
 const GET_PRODUCTS = 'GET_PRODUCTS'
+const GOT_CATEGORY_PRODUCTS = 'GOT_CATEGORY_PRODUCTS'
 
+//initial state
 const initialState = []
 
+//action creators
 const getProducts = products => ({ type: GET_PRODUCTS, products })
 
+const gotCategoryProducts = categoryProducts => ({
+  type: GOT_CATEGORY_PRODUCTS,
+  categoryProducts
+})
+
+//thunk creators
 export const getAllProducts = () => {
   return async (dispatch, _, { axios }) => {
     try {
       const { data } = await axios.get('api/products/')
       dispatch(getProducts(data))
-    } catch (error) {
-      console.error(error)
+    } catch (err) {
+      console.error(err)
     }
   }
 }
 
+export const getCategoryProducts = categoryId => {
+  return async (dispatch, _, { axios }) => {
+    try {
+      const { data } = await axios.get(`api/products/category/${categoryId}`)
+      dispatch(gotCategoryProducts(data))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+//reducer
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
-      return [...state, ...action.products]
+      return  [...state, ...action.products ]
+    case GOT_CATEGORY_PRODUCTS:
+      return [ ...action.categoryProducts ]
     default:
       return state
   }
--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -13,7 +13,10 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:productId', async (req, res, next) => {
   try {
-    const product = await Product.findAll({where: {id: req.params.productId}, include: {all: true}})
+    const product = await Product.findAll({
+      where: { id: req.params.productId },
+      include: { all: true }
+    })
     if (product) {
       //we are returning product[0] so that we only get a single object, instead of an array
       res.json(product[0])
@@ -25,3 +28,18 @@ router.get('/:productId', async (req, res, next) => {
   }
 })
 
+router.get('/category/:categoryId', async (req, res, next) => {
+  try {
+    console.log(req.params.categoryId)
+    if (req.params.categoryId === 'All Products') {
+      const products = await Product.findAll()
+      res.json(products)
+    } else {
+      const category = await Category.findById(req.params.categoryId)
+      const products = await category.getProducts()
+      res.json(products)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
